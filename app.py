23f03a8f8aba5cfc9ccc9be40a911826a34a57d4
Hassane Skikri <skikrihassane@gmail.com>
2024-04-08 22:38:44 +0000

    final editing en requirements

--- app.py
+++ app.py
@@ -64,6 +64,6 @@ def update(id):
 
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
